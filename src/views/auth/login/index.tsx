3c550f2d26b1bc4019a6f38e626f34bee043fec4
alpharii <alphari404@gmail.com>
2024-10-01 18:35:23 +0700

    install and use tailwind, scss

--- src/views/auth/login/index.tsx
+++ src/views/auth/login/index.tsx
@@ -1,5 +1,5 @@
 import Link from "next/link";
-import styles from "./Login.module.css";
+import styles from "./Login.module.scss";
 import { useRouter } from "next/router";
 
 export const LoginView = () => {
@@ -11,7 +11,7 @@ export const LoginView = () => {
     
     return (
         <div className={styles.login}>
-            <h1 className="big">Login Page</h1>
+            <h1 className="text-3xl font-bold">Login Page</h1>
             <button onClick={() => loginHandler()}>login</button>
             <p>Belum punya akun? <Link href="/auth/register">Register</Link></p>
         </div>
